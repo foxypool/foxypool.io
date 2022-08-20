711c13e58319c0e6bcc1025af0490983a11e44e0
Felix Brucker <felix@felixbrucker.com>
2022-08-20 09:19:10 +0200

    Remove flax, stai and hddcoin pools

--- src/app/pools.service.ts
+++ src/app/pools.service.ts
@@ -14,7 +14,7 @@ export class PoolsService {
       poolIdentifier: 'chia',
       isPoStPool: true,
       ticker: 'XCH',
-    },{
+    }, {
       coin: 'CHIA',
       name: 'Foxy-Pool CHIA (OG)',
       url: 'https://chia-og.foxypool.io',
@@ -23,15 +23,6 @@ export class PoolsService {
       isPoStPool: true,
       imageName: 'chia-og',
       ticker: 'XCH',
-    },{
-      coin: 'FLAX',
-      name: 'Foxy-Pool FLAX (OG)',
-      url: 'https://flax-og.foxypool.io',
-      color: '#e6ffe6',
-      poolIdentifier: 'flax-og',
-      isPoStPool: true,
-      imageName: 'flax-og',
-      ticker: 'XFX',
     }, {
       coin: 'CHIVES',
       name: 'Foxy-Pool CHIVES',
@@ -50,25 +41,7 @@ export class PoolsService {
       isPoStPool: true,
       imageName: 'chives-og',
       ticker: 'XCC',
-    },{
-      coin: 'HDDCOIN',
-      name: 'Foxy-Pool HDDCOIN (OG)',
-      url: 'https://hddcoin-og.foxypool.io',
-      color: '#ffffff',
-      poolIdentifier: 'hddcoin-og',
-      isPoStPool: true,
-      imageName: 'hddcoin-og',
-      ticker: 'HDD',
-    },{
-      coin: 'STAI',
-      name: 'Foxy-Pool STAI (OG)',
-      url: 'https://stai-og.foxypool.io',
-      color: '#363636',
-      poolIdentifier: 'stai-og',
-      isPoStPool: true,
-      imageName: 'stai-og',
-      ticker: 'STAI',
-    },{
+    }, {
       coin: 'BHD',
       name: 'Foxy-Pool BHD',
       url: 'https://bhd.foxypool.io',
